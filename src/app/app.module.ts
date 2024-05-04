e8df5872874243b063e549e5a6900698fce8a8f3
mursitkoc <uyemursit@yandex.com>
2024-05-04 23:35:03 +0300

    son değişiklikler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/product.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-  
   ],
   bootstrap: [AppComponent]
 })
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product} from './product';
 import { ProductfilterPipe } from './productfilter.pipe';
 import { FormsModule } from '@angular/forms';
+import { AlertifyService } from '../services/alertify.service';
 
 @Component({
   selector: 'app-product',
@@ -14,9 +15,9 @@ import { FormsModule } from '@angular/forms';
 })
 export class ProductComponent implements OnInit{
 
-  constructor(){}
+  constructor(private alertifyService:AlertifyService){}
   title="Ürün Listesi"
-  filterText="la";
+  filterText="";
   products : Product[]=
   [
     {id:1, name:"laptop", price:2500, categoryId:1, description: "asus zenbook", imageUrl:"https://images.unsplash.com/photo-1496181133206-80ce9b88a853?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
@@ -24,5 +25,13 @@ export class ProductComponent implements OnInit{
   ]
   ngOnInit(): void {
     
+  }
+  addToCart(product:Product){
+    // alertify.alert('Ready!');
+    this.alertifyService.success(product.name + " sepete eklendi");
+    this.alertifyService.error('Error!');
+    this.alertifyService.warning('Warning!'); 
+
+    // alert("sepete eklendi: "+product.name)
   }
 }
