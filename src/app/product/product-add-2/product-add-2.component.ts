9aac892932ad5758830fa47a4304a6f0cf9662e2
mursitkoc <uyemursit@yandex.com>
2024-05-11 23:30:53 +0300

    reaktif form bittti. çalışıyor

--- src/app/product/product-add-2/product-add-2.component.ts
+++ src/app/product/product-add-2/product-add-2.component.ts
@@ -1,38 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Product } from '../product';
+import { ProductService } from '../../services/product.service';
+import { CommonModule } from '@angular/common';
+import { CategoryService } from '../../services/category.service';
+import { AlertifyService } from '../../services/alertify.service';
+import { Category } from '../../category/category';
 
 @Component({
   selector: 'app-product-add-2',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './product-add-2.component.html',
-  styleUrl: './product-add-2.component.css'
+  styleUrl: './product-add-2.component.css',
+  providers: [ProductService, CategoryService, AlertifyService]
 })
 export class ProductAdd2Component implements OnInit {
-  constructor(private formBuilder:FormBuilder){}
+  constructor(
+    private formBuilder: FormBuilder,
+    private categoryService: CategoryService,
+    private alertifyService: AlertifyService,
+    private productService: ProductService,
+  ) { }
 
   productObj: Product = new Product();
-  
-  productAddForm = new FormGroup({
-    id: new FormControl<number>(4,Validators.required),
-    name: new FormControl<string>("",Validators.required),
-    price: new FormControl<number>(0,Validators.required),
-    categoryId: new FormControl<number>(0,Validators.required),
-    description: new FormControl<string>("",Validators.required),
-    imageUrl: new FormControl<string>("",Validators.required),
+  categories: Category[] = [];
+
+  productAddForm: FormGroup = new FormGroup({
+    id: new FormControl<number>(4, Validators.required),
+    name: new FormControl<string>("", Validators.required),
+    price: new FormControl<number>(0, Validators.required),
+    categoryId: new FormControl<number>(0, Validators.required),
+    description: new FormControl<string>("", Validators.required),
+    imageUrl: new FormControl<string>("", Validators.required),
   })
-  
 
   ngOnInit(): void {
-
+    this.categoryService.getCategories().subscribe(data => {
+      this.categories = data;
+    })
   }
 
-  add(){
-    if(this.productAddForm.valid){
-      this.productObj.id = this.productAddForm.value.id;
-      // this.productObj = Object.assign({}, this.productAddForm.value)
+  add() {
+    if (this.productAddForm.valid) {
+      // this.productObj.id = this.productAddForm.value.id;
+      this.productObj = Object.assign({}, this.productAddForm.value)
     }
+    this.productService.addProduct(this.productObj).subscribe(data => {
+      console.log(data)
+      // this.product = new Product(data.id, data.name, data.price, data.categoryId,data.description,data.imageUrl );
+      this.alertifyService.success(data.name + " added successfully")
+   }, error => {
+      console.log(error)
+      this.alertifyService.error(error)
+    })
   }
 
 }
